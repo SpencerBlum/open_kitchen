3934e22d33d451e94061fb55ca3951f517868f51
SpencerBlum <spencerblum@gmail.com>
2020-03-24 16:02:55 -0400

    Unfinished edits to post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,31 @@
 class CommentsController < ApplicationController
 
-    def new
+    
+    def new 
         @comment = Comment.new
     end
 
     def create
-        @comment = Comment.new(comment_params)
-
+        @comment = Comment.create(message: params[:comment][:message], user_id: session[:user_id])
+        byebug
+        redirect_to posts_path
     end
 
-    private
+    def edit
+        @comment = Comment.find(params[:id])
+    end
 
-    def comment_params
-        params.require(:comment).permit(:message, :user_id)
-      end
+    def update
+        @comment.find(params[:id])
+        @comment.update()
+        redirect_to posts_path
+    end
 
+    # private
+
+    # def comment_params
+    #     params.require(:comment).permit(:message, :user_id)
+    #   end
 
 
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,10 @@ class PostsController < ApplicationController
 
     def index
         @posts = Post.all
-        @comment = Comment.new
     end
+
+
+
+
 
 end
