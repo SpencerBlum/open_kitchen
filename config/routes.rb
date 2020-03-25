b182f346d51b45c054fd3e6118a306142c85bdfb
SpencerBlum <spencerblum@gmail.com>
2020-03-25 18:55:00 -0400

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,17 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users , only: [:new, :create]
   resources :restaurants
-  resources :comments, except: [:new, :edit, :update]
-  resources :posts
+  #resources :comments, except: [:new]
+  resources :posts do
+    resources :comments, only: [:index, :new, :create, :edit, :update]
+  end
 
-  get '/comments/new/:id', to: 'comments#new', as:'new_comment'
-  post '/comments/:id', to: 'comments#create'
+  #get '/posts/:post_id/comments/new', to :'comments#new', as: 'new_comment'
+  #get '/comments/new/:id', to: 'comments#new', as:'new_comment'
+  #post '/comments/:id', to: 'comments#create'
 
-  get '/comments/edit/:id', to: 'comments#edit', as:'edit_comment'
-  patch '/comments/:id', to: 'comments#update'
+  # get '/comments/edit/:id', to: 'comments#edit', as:'edit_comment'
+  # patch '/comments/:id', to: 'comments#update'
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
